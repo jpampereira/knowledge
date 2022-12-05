0696b6c0e9c16c5f1fdf780de86c74963114eeb2
jpampereira <jpampereira@gmail.com>
2022-12-04 21:09:00 -0300

    Comentários sobre o recurso de querys recursivas do postgres

--- backend/api/queries.js
+++ backend/api/queries.js
@@ -9,3 +9,17 @@ module.exports = {
     SELECT id FROM subcategories
   `,
 };
+
+/* - O recurso de recursividade do PostgreSQL nos permite construir uma árvore de registros.
+ *
+ * - No exemplo da aplicação, ele é utilizado para capturar todas as categorias que estão abaixo da
+ * informada na requisição da API.
+ * 
+ * - Ele cria uma tabela temporária e vai utilizando desta para realizar consultas recursivas. 
+ * Ao final, basta dar um SELECT em cima da tabela temporária que armazena todos os registros 
+ * obtidos dessas consultas.
+ * 
+ * - A query antes do UNION ALL é o passo inicial da recursão e a posterior são os passos
+ * seguintes. Os valores obtidos são armazenados na tabela temporária e uma nova query é
+ * realizada para cada um deles.
+ */
